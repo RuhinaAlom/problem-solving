9ab512b1ad3bea5888cac5f1940bdcc09c144fba
RuhinaAlom <ruhinaalomayesha>
2025-08-16 11:43:21 +0600

    third update

--- variable.js
+++ variable.js
@@ -1,3 +1,11 @@
+let a = 5;
+let b = 10;
+
+console.log("Before swap: a =", a, "b =", b);
+a = a + b;
+b = a - b;
+a = a - b;
+console.log("After swap: a =", a, "b =", b);
 
 //Convert Celsius fahrenheit
 let celsius = 25;
