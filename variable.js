5d761cab11d1a4f5d9e74135d1e60e03a5e64946
RuhinaAlom <ruhinaalomayesha>
2025-08-16 12:01:39 +0600

    fourth update

--- variable.js
+++ variable.js
@@ -12,3 +12,38 @@ let celsius = 25;
 let fahrenheit = (celsius * 9 / 5) + 32;
 console.log(celsius + " c is equal to " + fahrenheit + " F");
 
+
+// Check if a number is Even or odd
+
+let num = 7;
+if (num % 2 === 0) {
+    console.log(num + "is even");
+
+}
+else {
+    console.log(num + " is odd");
+}
+
+// find maximum of two numbers
+
+let x = 15;
+let y = 20;
+
+
+let max;
+if (x > y) {
+    max = x;
+
+}
+else {
+    max = y;
+}
+console.log("Maximum number is: " + max);
+
+//using the ternary operator ?
+let num1 = 50;
+let num2 = 170;
+console.log("Before swap: num1 =", num1, "num1 =", num2);
+let maximun = (num1 > num2) ? num1 : num2;
+
+console.log("Maximum number is: ", maximun); 
