cb94a704306497c8c5ca73d76648c87c7464ebaf
RuhinaAlom <ruhinaalomayesha>
2025-08-16 11:39:08 +0600

    second update

--- variable.js
+++ variable.js
@@ -1,8 +1,6 @@
-let a = 5;
-let b = 10;
 
-console.log("Before swap: a =", a, "b =", b);
-a = a + b;
-b = a - b;
-a = a - b;
-console.log("After swap: a =", a, "b =", b);
+//Convert Celsius fahrenheit
+let celsius = 25;
+let fahrenheit = (celsius * 9 / 5) + 32;
+console.log(celsius + " c is equal to " + fahrenheit + " F");
+
